61b46b6c20258d4de420e137ce87fa155df2bf4c
Mateusz <kwiatkiwoknie@gmail.com>
2015-10-30 17:59:18 +0100

    Static page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :pages
   root 'articles#index'
   get 'articles/popular' => 'articles#popular'
   get 'articles/archive' => 'articles#archive'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029142157) do
+ActiveRecord::Schema.define(version: 20151030165828) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20151029142157) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
