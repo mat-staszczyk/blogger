829794a4c3d4b02271232b28511ddb03a0d900a4
Mateusz <kwiatkiwoknie@gmail.com>
2015-10-31 22:52:56 +0100

    Final fixes

--- app/models/text_content.rb
+++ app/models/text_content.rb
@@ -1,15 +1,17 @@
 module TextContent
+  extend ActiveSupport::Concern
+
   def word_count
     body.split.count
   end
 
   module ClassMethods
-    def self.total_word_count
+    def total_word_count
       all.inject(0) { |total, a| total += a.word_count }
     end
   end
 
-  def self.included(including_class)
-    including_class.extend ClassMethods
+  included do
+    has_one :moderator_approval, as: :content
   end
 end
